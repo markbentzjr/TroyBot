b84ff27fbaa1ae19be5d4d1180e8f8c54777db5a
daftmaple <35413259+daftmaple@users.noreply.github.com>
2018-11-22 17:46:02 +0700

    I screwed a lot

--- src/code.py
+++ src/code.py
@@ -4,46 +4,76 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 
-class TutorialBot(BaseAgent):
-    def __init__(self, name, team, index):
-        super().__init__(name, team, index)
-        self.controller = SimpleControllerState()
+class PythonExample(BaseAgent):
 
-        # Game values
-        self.bot_pos = None
-        self.bot_yaw = None
-
-    def aim(self, target_x, target_y):
-        angle_between_bot_and_target = math.atan2(target_y - self.bot_pos.y,
-                                                target_x - self.bot_pos.x)
-
-        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw
-
-        # Correct the values
-        if angle_front_to_target < -math.pi:
-            angle_front_to_target += 2 * math.pi
-        if angle_front_to_target > math.pi:
-            angle_front_to_target -= 2 * math.pi
-
-        if angle_front_to_target < math.radians(-10):
-            # If the target is more than 10 degrees right from the centre, steer left
-            self.controller.steer = -1
-        elif angle_front_to_target > math.radians(10):
-            # If the target is more than 10 degrees left from the centre, steer right
-            self.controller.steer = 1
-        else:
-            # If the target is less than 10 degrees from the centre, steer straight
-            self.controller.steer = 0
+    def initialize_agent(self):
+        # This runs once before the bot starts up
+        self.controller_state = SimpleControllerState()
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        # Update game data variables
-        self.bot_yaw = packet.game_cars[self.team].physics.rotation.yaw
-        self.bot_pos = packet.game_cars[self.index].physics.location
+        ball_location = Vector2(
+            packet.game_ball.physics.location.x, packet.game_ball.physics.location.y)
 
-        ball_pos = packet.game_ball.physics.location
-        self.aim(ball_pos.x, ball_pos.y)
+        my_car = packet.game_cars[self.index]
+        car_location = Vector2(my_car.physics.location.x,
+                               my_car.physics.location.y)
+        car_direction = get_car_facing_vector(my_car)
+        car_to_ball = ball_location - car_location
 
-        self.controller.throttle = 1
-        self.controller.boost = True
+        steer_correction_radians = car_direction.correction_to(car_to_ball)
 
-        return self.controller
+        if steer_correction_radians > 0:
+            # Positive radians in the unit circle is a turn to the left.
+            turn = -1.0  # Negative value for a turn to the left.
+        else:
+            turn = 1.0
+
+        if abs(steer_correction_radians) > math.pi / 2:
+            self.controller_state.boost = False
+            self.controller_state.handbrake = True
+        else:
+            self.controller_state.boost = True
+            self.controller_state.handbrake = False
+
+        self.controller_state.throttle = 1.0
+        self.controller_state.steer = turn
+
+        return self.controller_state
+
+
+class Vector2:
+    def __init__(self, x=0, y=0):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __add__(self, val):
+        return Vector2(self.x + val.x, self.y + val.y)
+
+    def __sub__(self, val):
+        return Vector2(self.x - val.x, self.y - val.y)
+
+    def correction_to(self, ideal):
+        # The in-game axes are left handed, so use -x
+        current_in_radians = math.atan2(self.y, -self.x)
+        ideal_in_radians = math.atan2(ideal.y, -ideal.x)
+
+        correction = ideal_in_radians - current_in_radians
+
+        # Make sure we go the 'short way'
+        if abs(correction) > math.pi:
+            if correction < 0:
+                correction += 2 * math.pi
+            else:
+                correction -= 2 * math.pi
+
+        return correction
+
+
+def get_car_facing_vector(car):
+    pitch = float(car.physics.rotation.pitch)
+    yaw = float(car.physics.rotation.yaw)
+
+    facing_x = math.cos(pitch) * math.cos(yaw)
+    facing_y = math.cos(pitch) * math.sin(yaw)
+
+    return Vector2(facing_x, facing_y)
