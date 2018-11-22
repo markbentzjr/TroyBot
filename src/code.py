3d7ce70225b1dea9c1a6dd56db05d4cfe7912355
daftmaple <35413259+daftmaple@users.noreply.github.com>
2018-11-23 02:01:19 +0700

    Building the template for code.py

--- src/code.py
+++ src/code.py
@@ -1,79 +1,36 @@
 import math
+import time # For timings
 
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
+# Personal codes (classes)
+from position import Vector3
 
-class PythonExample(BaseAgent):
+class TroyBot(BaseAgent):
 
     def initialize_agent(self):
-        # This runs once before the bot starts up
+        # This state is initialized once before the bot starts up
         self.controller_state = SimpleControllerState()
+        self.clear_state()
+        
+        # Game state
+        self.action = 0
+        
+
+    # Clears bot controller state
+    def clear_state(self):
+        # Read https://github.com/RLBot/RLBotPythonExample/wiki/Input-and-Output-Data for full documentation
+        self.controller_state.throttle = 0
+        self.controller_state.steer = 0
+        self.controller_state.pitch = 0
+        self.controller_state.yaw = 0
+        self.controller_state.roll = 0
+        self.controller_state.boost = False
+        self.controller_state.jump = False
+        self.controller_state.handbrake = False
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        ball_location = Vector2(
-            packet.game_ball.physics.location.x, packet.game_ball.physics.location.y)
-
-        my_car = packet.game_cars[self.index]
-        car_location = Vector2(my_car.physics.location.x,
-                               my_car.physics.location.y)
-        car_direction = get_car_facing_vector(my_car)
-        car_to_ball = ball_location - car_location
-
-        steer_correction_radians = car_direction.correction_to(car_to_ball)
-
-        if steer_correction_radians > 0:
-            # Positive radians in the unit circle is a turn to the left.
-            turn = -1.0  # Negative value for a turn to the left.
-        else:
-            turn = 1.0
-
-        if abs(steer_correction_radians) > math.pi / 2:
-            self.controller_state.boost = False
-            self.controller_state.handbrake = True
-        else:
-            self.controller_state.boost = True
-            self.controller_state.handbrake = False
-
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = turn
+        self.clear_state()
 
         return self.controller_state
-
-
-class Vector2:
-    def __init__(self, x=0, y=0):
-        self.x = float(x)
-        self.y = float(y)
-
-    def __add__(self, val):
-        return Vector2(self.x + val.x, self.y + val.y)
-
-    def __sub__(self, val):
-        return Vector2(self.x - val.x, self.y - val.y)
-
-    def correction_to(self, ideal):
-        # The in-game axes are left handed, so use -x
-        current_in_radians = math.atan2(self.y, -self.x)
-        ideal_in_radians = math.atan2(ideal.y, -ideal.x)
-
-        correction = ideal_in_radians - current_in_radians
-
-        # Make sure we go the 'short way'
-        if abs(correction) > math.pi:
-            if correction < 0:
-                correction += 2 * math.pi
-            else:
-                correction -= 2 * math.pi
-
-        return correction
-
-
-def get_car_facing_vector(car):
-    pitch = float(car.physics.rotation.pitch)
-    yaw = float(car.physics.rotation.yaw)
-
-    facing_x = math.cos(pitch) * math.cos(yaw)
-    facing_y = math.cos(pitch) * math.sin(yaw)
-
-    return Vector2(facing_x, facing_y)
---
+++ src/position.py
@@ -0,0 +1,13 @@
+import math
+
+class Vector3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __add__(self, val):
+        return Vector3(self.x + val.x, self.y + val.y, self.z + val.z)
+
+    def __sub__(self, val):
+        return Vector3(self.x - val.x, self.y - val.y, self.z - val.z)
