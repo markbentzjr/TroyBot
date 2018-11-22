800300e05b51c8027ce567e136abc8f742a6e9b4
Winston Smith <35413259+daftmaple@users.noreply.github.com>
2018-11-22 11:52:45 +0700

    Initial push

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# TroyBot
+# Troy
+This bot is dedicated to Regan, the most toxic RL player I have ever known. This bot name was taken from Shroud's dog, Troy the German Shepherd - Siberian Husky mix
+
+### Plain instructions
+
+1. Make sure you've installed [Python 3.6 64 bit](https://www.python.org/ftp/python/3.6.5/python-3.6.5-amd64.exe). During installation:
+   - Select "Add Python to PATH"
+   - Make sure pip is included in the installation
+2. Open Rocket League
+3. Download or clone this repository
+3. In the files from the previous step, find and double click on run-gui.bat
+4. Click the 'Run' button
+
+
+## Notes
+
+- Bot behavior is controlled by `src/code.py`
+
+- Bot appearance is controlled by `src/appearance.cfg`
+
+- `run.py` runs the whole match execution, while all "bots code" is managed under `rlbot.cfg`
+
+- Inside `rlbot.cfg`, bots code are included inside `src/config.cfg` which eventually links to `src/appearance.cfg` and `src/code.py`, where the appearance and behaviour is controlled.
+
+- Hence, do not modify anything apart from `src/~`
+
+## Important note
+Always use working branch for any future works, and release branch to release the working bot
+
+See https://github.com/RLBot/RLBotPythonExample/wiki for documentation and tutorials.
---
+++ src/code.py
@@ -0,0 +1,49 @@
+import math
+
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+
+class TutorialBot(BaseAgent):
+    def __init__(self, name, team, index):
+        super().__init__(name, team, index)
+        self.controller = SimpleControllerState()
+
+        # Game values
+        self.bot_pos = None
+        self.bot_yaw = None
+
+    def aim(self, target_x, target_y):
+        angle_between_bot_and_target = math.atan2(target_y - self.bot_pos.y,
+                                                target_x - self.bot_pos.x)
+
+        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw
+
+        # Correct the values
+        if angle_front_to_target < -math.pi:
+            angle_front_to_target += 2 * math.pi
+        if angle_front_to_target > math.pi:
+            angle_front_to_target -= 2 * math.pi
+
+        if angle_front_to_target < math.radians(-10):
+            # If the target is more than 10 degrees right from the centre, steer left
+            self.controller.steer = -1
+        elif angle_front_to_target > math.radians(10):
+            # If the target is more than 10 degrees left from the centre, steer right
+            self.controller.steer = 1
+        else:
+            # If the target is less than 10 degrees from the centre, steer straight
+            self.controller.steer = 0
+
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        # Update game data variables
+        self.bot_yaw = packet.game_cars[self.team].physics.rotation.yaw
+        self.bot_pos = packet.game_cars[self.index].physics.location
+
+        ball_pos = packet.game_ball.physics.location
+        self.aim(ball_pos.x, ball_pos.y)
+
+        self.controller.throttle = 1
+        self.controller.boost = True
+
+        return self.controller
