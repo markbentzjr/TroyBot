f1ebef07382f663beef8de5bc61c0ea2029d5963
daftmaple <35413259+daftmaple@users.noreply.github.com>
2018-11-24 04:14:17 +0700

    Update code.py

--- src/code.py
+++ src/code.py
@@ -26,7 +26,7 @@ class TroyBot(BaseAgent):
         # Action  3: Collect boost
         # Action  4: Demo
         # Action 99: Ballchase
-
+        self.tick = 0
         self.target = None
 
         self.field_info = self.get_field_info()
@@ -88,28 +88,33 @@ class TroyBot(BaseAgent):
 
     def goto_boost(self, packet):
         boost_location = []
-
-        for i in self.field_info.boost_pads:
-            if i.is_full_boost == True:
-                boost_location.append(Vector3(i.location.x, i.location.y, i.location.z))
+        pads = self.field_info.boost_pads
+        for i in range(len(pads)):
+            if (pads[i].is_full_boost == True) and (packet.game_boosts[i].is_active == True):
+            #if (i.is_full_boost == True):
+                boost_location.append(Vector3(pads[i].location.x, pads[i].location.y, pads[i].location.z))
 
 
         own_car = packet.game_cars[self.index]
         own_car_location = Vector3(own_car.physics.location.x,
                                own_car.physics.location.y, own_car.physics.location.z)
         #print(own_car.physics.location.x, own_car.physics.location.y, own_car.physics.location.z)
-        # print(boost_location)
-        boost_choose = boost_location[0]
-        distance = own_car_location.real_distance(boost_choose)
+        
+        boost_choose_location = boost_location[0]
+        distance = own_car_location.real_distance(boost_choose_location)
         for i in range(len(boost_location)):
             boost = boost_location[i]
+            """ print(i)
+            print(own_car_location.real_distance(boost))
+            print("\n") """
             if (own_car_location.real_distance(boost) < distance):
                 distance = own_car_location.real_distance(boost_location[i])
-                boost_choose = boost_location[i]
-        # print(i)
+                boost_choose_location = boost_location[i]
+        
         own_dir = facing(own_car)
         #print(boost_choose.x, boost_choose.y, boost_choose.z)
-        car_to_target = boost_choose - own_dir
+        car_to_target = boost_choose_location - own_car_location
+        #print(car_to_target.x, car_to_target.y)
         rad = own_dir.correction_2D(car_to_target)
         #print(rad)
 
@@ -122,7 +127,7 @@ class TroyBot(BaseAgent):
         
         self.controller_state.throttle = 1.0
         self.controller_state.steer = turn
-
+        self.tick += 1
         return self
 
     def kickoff(self, packet):
