2c160336a69d4d202dc8dcaeaba0fae74b3f0bc4
daftmaple <35413259+daftmaple@users.noreply.github.com>
2018-11-25 15:58:43 +0700

    Update code.py
    A bit of update

--- src/code.py
+++ src/code.py
@@ -28,6 +28,7 @@ class TroyBot(BaseAgent):
         # Action  2: Attack
         # Action  3: Collect boost
         # Action  4: Demo
+        # Action 95: Get into position to ballchase
         # Action 99: Ballchase
 
         #self.tick = 0
@@ -82,18 +83,24 @@ class TroyBot(BaseAgent):
 
         return self.controller_state
 
+# Decision function
     def check_action(self, packet):
         # For 1 opponent
         own_car = packet.game_cars[self.index]
-        if (own_car.boost == 0.0):
+        
+        if self.check_kickoff(packet) == True:
+            self.action = 0
+        elif (own_car.boost == 0.0):
             self.action = 3
         else:
             self.action = 99
-
+        #print(self.check_kickoff(packet))
+        print(self.action)
         # For 2 opponents
         # For 3/4 opponents
         return self.action
 
+# Quickchat
     def quick_chat(self):
         #print(math.floor(time.time()))
         if (math.floor(time.time()) % 32 == 0) and math.floor(time.time()) != self.chat:
@@ -108,7 +115,8 @@ class TroyBot(BaseAgent):
         elif (math.floor(time.time()) % 32 == 24) and math.floor(time.time()) != self.chat:
             self.send_quick_chat(QuickChats.CHAT_EVERYONE, QuickChats.Compliments_Thanks)
             self.chat = math.floor(time.time())
-        
+
+# Action 3: Boost
     def goto_boost(self, packet):
         boost_location = []
         pads = self.field_info.boost_pads
@@ -158,9 +166,24 @@ class TroyBot(BaseAgent):
         #self.tick += 1
         return self
 
+# Action 0: Kickoff
+    def check_kickoff(self, packet):
+        ball = packet.game_ball.physics
+        #print(ball.velocity.x, ball.velocity.y, ball.velocity.z)
+        Condition0Loc = Vector3(0, 0, 93)
+        Condition0Vel = Vector3(0, 0, 0)
+        Location = Vector3(ball.location.x, ball.location.y, math.ceil(ball.location.z))
+        Velocity = Vector3(ball.velocity.x, ball.velocity.y, ball.velocity.z)
+        if Location.real_distance(Condition0Loc) < 1 and Velocity.real_distance(Condition0Vel) < 1:
+            return True
+        return False
+
     def kickoff(self, packet):
+        self = self.ballchase(packet)
+        self.controller_state.boost = True
         return self
 
+# Action 99: Ballchase
     def ballchase(self, packet):
         ball_location = Vector3(packet.game_ball.physics.location.x,
                                 packet.game_ball.physics.location.y, packet.game_ball.physics.location.z)
@@ -196,12 +219,22 @@ class TroyBot(BaseAgent):
                 self.controller_state.boost = True
             else:
                 self.controller_state.handbrake = True
+                self.controller_state.boost = False
         else:
             self.controller_state.boost = False
         return self
-    
+
+# Action 4: Demolitions
     def demo(self, packet):
         return self
+
+# Action 95: Get into position to ballchase
+    def positioning_ballchase(self, packet):
+
+        return self
+
+    def check_position(self, packet):
+        return 0
 
 def facing(car):
     pitch = float(car.physics.rotation.pitch)
