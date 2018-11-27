71c2474a57f28b7e300212d338a9d7a4dcc8895e
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-27 11:43:18 -0600

    Update shooting.py

--- src/shooting.py
+++ src/shooting.py
@@ -1,10 +1,24 @@
 import math
 
 from position import *
+from rlbot.utils.structures.game_data_struct import GameTickPacket
 
-
-#If ball in enemy half
-
-#shooting state
+#get ball location
+ball_location_y =  packet.game_ball.physics.location.y
+                   
+  
+#If ball in enemy half change state team Blue
+if packet.game_cars[agent.index].team == 0 and ball_location_y > 0
+  state = strategy.shooting
+else
+  state = strategy.defense
+  
+#If ball in enemy half change state team Orange
+if packet.game_cars[agent.index].team == 1 and ball_location_y < 0
+  state = strategy.shooting
+else
+  state = strategy.defense
 
 #shooting code
+def shooting(agent, packet)
+  
